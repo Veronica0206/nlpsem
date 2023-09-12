8020dc61e5b904498dfacd24a8bf7cda2504ddb1
Jin (Veronica) Liu <Veronica.Liu0206@gmail.com>
2023-09-12 12:15:15 -0400

    updated to V0.3

---
+++ R/global_vars.R
@@ -0,0 +1,30 @@
+# global_vars.R
+
+# Inform R of our "undefined" global variables
+utils::globalVariables(c(
+  "BL_mean", "M_alpha", "M_alpha0", "M_alpha1", "M_alpha2", "M_alphar", "M_mean",
+  "M_mug", "M_psi00", "M_psi01", "M_psi11", "M_psi_r", "Omega", "X_mean", "X_mean0",
+  "X_mean1", "X_mean2", "X_meanr", "X_mueta0", "X_mueta1", "X_mug", "X_psi0",
+  "X_psi00", "X_psi01", "X_psi11", "Y_acc_ratio", "Y_alpha", "Y_alpha0", "Y_alpha1",
+  "Y_alpha2", "Y_alpha_s", "Y_alphar", "Y_knot", "Y_mean", "Y_mean0", "Y_mean_s",
+  "Y_mueta0", "Y_mueta0s", "Y_mueta1", "Y_mueta1s", "Y_mueta2", "Y_mueta2s", "Y_mug",
+  "Y_psi0", "Y_psi00", "Y_psi00s", "Y_psi01", "Y_psi01s", "Y_psi02", "Y_psi02s",
+  "Y_psi0g", "Y_psi0gs", "Y_psi11", "Y_psi11s", "Y_psi12", "Y_psi12s", "Y_psi1g",
+  "Y_psi1gs", "Y_psi22", "Y_psi22s", "Y_psi2g", "Y_psi2gs", "Y_psi_r", "Y_psi_s",
+  "Y_psigg", "Y_psiggs", "Y_slp_ratio", "Ychg_bl_m", "Ychg_bl_v", "Ychg_inv_m",
+  "Ychg_inv_v", "Yslp_m", "Yslp_v", "beta0TVC", "beta1TVC", "beta2TVC", "betaM0",
+  "betaM0Y0", "betaM0Y1", "betaM1", "betaM1Y1", "betaM1Y2", "betaM1Yr", "betaM2",
+  "betaM2Y2", "betaMr", "betaMrY2", "betaMrYr", "betaX0M0", "betaX0M1", "betaX0Y0",
+  "betaX0Y1", "betaX1M1", "betaX1M2", "betaX1Mr", "betaX1Y1", "betaX1Y2", "betaX1Yr",
+  "betaX2M2", "betaX2Y2", "betaXrM2", "betaXrMr", "betaXrY2", "betaXrYr", "betaY0",
+  "betaY1", "betaY2", "betaYr", "beta_TIC", "beta_my", "beta_s", "beta_xm", "beta_xy",
+  "betagTVC", "classbeta", "func", "grad", "knot", "mediator", "mediator_00", "mediator_000",
+  "mediator_001", "mediator_01", "mediator_011", "mediator_11", "mediator_111",
+  "mediator_112", "mediator_11r", "mediator_12", "mediator_122", "mediator_1r",
+  "mediator_1r2", "mediator_1rr", "mediator_22", "mediator_222", "mediator_r2",
+  "mediator_r22", "mediator_rr", "mediator_rr2", "mediator_rrr", "muX", "mux",
+  "objective", "r_loads", "s_loads", "total", "weightsV", "seed", "fit", "statusCode",
+  "name", "Estimate", "ID", "time", "value", "Class", "Model", "No_Params", "X.2ll",
+  "Prop", "matches", "pivot_wider"
+))
+
